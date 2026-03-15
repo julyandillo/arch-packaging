d6f3b1893357f6cfc3db5eb6e36a0c6789919baf
Julián Bajo <bajoherrero.julian@proton.me>
2026-03-15 17:27:17 +0100

    Agrega tree y man-pages-es

--- tools.sh
+++ tools.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
 yay -S --needed --noconfirm \
-    git \
-    eza \
-    fzf \
-    caligula \
-    ventoy-bin \
-    xarchiver \
-    ripgrep \
-    bat \
-    filezilla \
-    tailspin \
-    remmina \
-    freerdp \
-    rustdesk-bin \
-    tailtray \
-    anydesk-bin \
-    tailscale \
-    tail-tray \
-    stow \
-    starship \
-    okular \
-    fish \
-    wl-clipboard \
-    btop \
-    zapzap \
-    rsync \
-    vlc \
-    ghostty \
-    kitty \
-    kitty-term-info
+  git \
+  eza \
+  fzf \
+  caligula \
+  ventoy-bin \
+  xarchiver \
+  ripgrep \
+  bat \
+  filezilla \
+  tailspin \
+  remmina \
+  freerdp \
+  rustdesk-bin \
+  tailtray \
+  anydesk-bin \
+  tailscale \
+  tail-tray \
+  stow \
+  starship \
+  okular \
+  fish \
+  wl-clipboard \
+  btop \
+  zapzap \
+  rsync \
+  vlc \
+  ghostty \
+  kitty \
+  kitty-term-info \
+  tree \
+  man-pages-es
 
 sudo systemctl enable --now tailscaled
